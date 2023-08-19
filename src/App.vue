1da25e4e04efe7c40e2edb26daaa1368dfe769e2
Marco <marcowky@qq.com>
2023-08-20 02:47:16 +0800

    修复 排版问题

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="center-box main-box">
-    <div class="tltle"/>
+    <div class="tltle" />
     <div class="context-box">
       <router-view />
     </div>
-    <div class="footbar"/>
   </div>
 </template>
 
@@ -14,7 +13,7 @@
 
 <style scoped>
 .main-box {
-  height: 98vh;
+  height: 100vh;
   background-image: url("./assets/images/background.png");
   background-size: cover;
   background-position: center;
@@ -22,7 +21,7 @@
 
 .context-box {
   position: absolute;
-  top: 30%;
+  top: 25%;
   width: 100%;
   background-image: url("./assets/images/context.png");
   background-size: 100% 100%;
@@ -31,7 +30,7 @@
 
 .tltle {
   position: absolute;
-  top: 8%;
+  top: 5%;
   width: 300px;
   height: 163px;
   background-image: url("./assets/images/title.png");
@@ -39,15 +38,6 @@
   background-position: center;
 }
 
-.footbar {
-  position: absolute;
-  bottom: 0%;
-  width: 400px;
-  height: 50px;
-  background-image: url("./assets/images/footbar.png");
-  background-size: 100% 100%;
-  background-position: center;
-}
 el-button {
   touch-action: manipulation;
 }
