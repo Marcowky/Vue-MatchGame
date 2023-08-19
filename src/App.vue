44d7c3f25e9b76789115a40d11b4a8f57034cc11
Marco <marcowky@qq.com>
2023-08-20 02:28:31 +0800

    实现 所有样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="main-box">
-    <router-view />
+  <div class="center-box main-box">
+    <div class="tltle"/>
+    <div class="context-box">
+      <router-view />
+    </div>
+    <div class="footbar"/>
   </div>
 </template>
 
@@ -10,12 +14,40 @@
 
 <style scoped>
 .main-box {
-  height: 100vh;
+  height: 98vh;
   background-image: url("./assets/images/background.png");
   background-size: cover;
   background-position: center;
 }
 
+.context-box {
+  position: fixed;
+  top: 30%;
+  width: 100%;
+  background-image: url("./assets/images/context.png");
+  background-size: 100% 100%;
+  background-position: center;
+}
+
+.tltle {
+  position: fixed;
+  top: 8%;
+  width: 300px;
+  height: 163px;
+  background-image: url("./assets/images/title.png");
+  background-size: 100% 100%;
+  background-position: center;
+}
+
+.footbar {
+  position: fixed;
+  bottom: 0%;
+  width: 400px;
+  height: 50px;
+  background-image: url("./assets/images/footbar.png");
+  background-size: 100% 100%;
+  background-position: center;
+}
 el-button {
   touch-action: manipulation;
 }
